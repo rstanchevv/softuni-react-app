14cbf92a0e4e4ae229867ea651b4eb5d9c6f6dd8
Radoslav Stanchev <rstanchev93@abv.bg>
2023-12-08 12:36:00 +0200

    added more infromation to Profile page and added partial readme file

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# React + Vite
+React Softuni App is an application that has been developed for the exam at the end of the React course.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# OfferHub
 
-Currently, two official plugins are available:
+The application represents a SPA application where users could see/post/edit/delete/buy offers.
+For the purpose of Front-End development it was used React.js.
+For the purpose of storing information and authentication/authorization it was used Firestore.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Roles
+
+Non-authenticated users can visit the application and are authorized to see:
+
+    * Register - allows the user to Register and perform authenticated operations afterwards
+    * Login - allows the user to Login with an existing account and perform authenticated operations afterwards
+    * Offers - allows the user to see all open offers that have not been bought yet
+    * Offer details - allows the user to see more details of an offer such as its category, price and details
+
+Authenticated users (non-owners) can visit the application and are authorized to see and use:
+
+    * Profile - allows the user to see who is logged in currently as well as the offers that have been bought by the current user
+    * Offers - allows the user to see all open offers that have not been bought yet
+    * Offers details - allows the user to see more details of an offer such as its category, price and detail as well as to buy it
+    * Offer details buy - functionality that allows the user to buy a specific item. Once bought, the offer is no longer displayed in the "Offers" and could be found under the buyer's profile
+    * Bought offer delete - funcionaliy that allows the buyer to delete the offer from their bought list.
+
+Authenticated users (owners) can visit the application and are authorized to see and use:
+
+    * Profile - allows the user to see who is logged in currently as well as the offers that have been bought by the current user
+    * Offers - allows the user to see all open offers that have not been bought yet
+    * Offers details - allows the user to see more details of an offer such as its category, price and detail as well as to edit or delete it
+    * Offer details edit - functionality that allows the user to change the details of an offer that is under their ownership;
+    * Offer details delete - functionality that allows the user to delete an offer that is under their ownership;
+    * Bought offer delete - funcionaliy that allows the buyer to delete the offer from their bought list.
+    
--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -6,39 +6,50 @@ import { Offer } from "../Offers/OfferComponent";
 import { NoOffersYet } from "../Offers/NoOffersYet";
 
 export const Profile = () => {
-    const { authInfo } = useContext(AuthContext)
-    const [loadingSpinnerState, setLoadingSpinner] = useState(false);
-    const [showNoOffers, setShowNoOffers] = useState(false)
-    const [offers, setNewOffers] = useState([]);
-    useEffect(() => {
-      setLoadingSpinner(true);
-      getAllOffers()
-      .then(res => {
-        const boughtOffers = res.filter(x => x.data.boughtBy == authInfo.uid)
-        setNewOffers(boughtOffers);
-        if (res.filter(x => x.data.boughtBy == authInfo.uid).length < 1){
-          setLoadingSpinner(false)
-          setShowNoOffers(true)
-        } else {
-          setLoadingSpinner(false)
-          setShowNoOffers(false)
-        }
-      })
-    }, []);
-  
-    return (
-      <>
-        <div className="container-fluid tm-container-content tm-mt-60">
-          <div className="row mb-4">
-            <h2 className="col-6 tm-text-primary">Bought products</h2>
-          </div>
+  const { authInfo } = useContext(AuthContext);
+  const [loadingSpinnerState, setLoadingSpinner] = useState(false);
+  const [showNoOffers, setShowNoOffers] = useState(false);
+  const [offers, setNewOffers] = useState([]);
+  useEffect(() => {
+    setLoadingSpinner(true);
+    getAllOffers().then((res) => {
+      const boughtOffers = res.filter((x) => x.data.boughtBy == authInfo.uid);
+      setNewOffers(boughtOffers);
+      if (res.filter((x) => x.data.boughtBy == authInfo.uid).length < 1) {
+        setLoadingSpinner(false);
+        setShowNoOffers(true);
+      } else {
+        setLoadingSpinner(false);
+        setShowNoOffers(false);
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div class="card" style={{width: '12rem'}}>
+        <img class="card-img-top" src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="Card image cap" />
+        <div class="card-body">
+          <p class="card-text" style={{fontWeight: 'bold'}}>
+            Current user: <p style={{color: 'green'}}>{authInfo.email}</p>
+          </p>
         </div>
-        <div className="row tm-mb-90 tm-gallery">
-          {loadingSpinnerState && <LoadingSpinner />}
-          {showNoOffers ? <NoOffersYet/> : offers.map((offer) => (
-            <Offer key={offer.id} {...offer.data} id={offer.id}/>
-          ))}
+      </div>
+      <div className="container-fluid tm-container-content tm-mt-60">
+        <div className="row mb-4">
+          <h2 className="col-6 tm-text-primary">Bought products</h2>
         </div>
-      </>
-    );
-  };
+      </div>
+      <div className="row tm-mb-90 tm-gallery">
+        {loadingSpinnerState && <LoadingSpinner />}
+        {showNoOffers ? (
+          <NoOffersYet />
+        ) : (
+          offers.map((offer) => (
+            <Offer key={offer.id} {...offer.data} id={offer.id} />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
